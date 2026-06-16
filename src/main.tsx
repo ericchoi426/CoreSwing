103618d75eea705e46e0bb06286c17cd32597d61
ericchoi426 <ericchoi426@gmail.com>
2026-06-16 14:28:17 +0900

    bug fetch2

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '398384748843-m6hh91i7bk9f2o2i0me8s542ikltnqui.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
